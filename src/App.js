76d2ad4259086a09f3683561bfdbba02d888bf62
ManhTrinhNguyen <nguyenmanhtrinh17041998@gmail.com>
2023-01-10 23:54:40 -0500

    Updated final Score

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
-import Timer from "./components/Timer";
 import "./app.css"
 
 function App() {
-  //const [startGame, setStartGame] = useState(true)
   const [dataQuiz, setDataQuiz] = useState([])
   const [quizIndex, setQuizIndex] = useState(0)
   const [score, setScore] = useState(0)
   const [selectAnswer, setSelectAnswer] = useState("")
   const [className, setClassName] = useState("answer")
-  const [switchNextQuestion, setSwitchNextQuestion] = useState(false)
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5")
@@ -36,11 +33,13 @@ function App() {
     })
   },[])
  
-
+  // Answer List
   const answer = dataQuiz.map(quiz => {
     return quiz.answers
   })
+  // Correct Answer
   const correctAnswer = dataQuiz[quizIndex]?.correctAnswer
+
   function handleClick(answer) {
     setSelectAnswer(correctAnswer)
     if (answer === correctAnswer) {
@@ -55,8 +54,7 @@ function App() {
     setScore(0)
     setQuizIndex(0)
   }
-  
-  console.log(switchNextQuestion)
+
  
   return (
     <div className="App">
@@ -64,18 +62,17 @@ function App() {
           <div className="score-container">
             <h1>Correct Answer {score}/5</h1>
           </div>
-          <div className="timer"><Timer quizIndex={quizIndex} setSwitchNextQuestion={setSwitchNextQuestion}/></div>
           <div className="question-container">
             <h1 className="question">{dataQuiz[quizIndex]?.question}</h1>
           </div>
-          <div className="answer-container">{answer[quizIndex]?.map(item => (
-            <button className={selectAnswer === item ? "answer correct" : "answer wrong"} onClick={() => {
+          <div className="answer-container">{answer[quizIndex]?.map((item, index) => (
+            <button key={index} className={selectAnswer === item ? "answer correct" : "answer wrong"} onClick={() => {
              handleClick(item)
             }}>{item}</button>
           ))}</div>
           {quizIndex === 5 ? 
           <div className="scoreContainer">
-              <h1>Your Score is {score}/5</h1>
+              <h1 className="finalScore">Your Score is {score}/5</h1>
               <button onClick={playAgain} className="nextQuestionBtn">Play Again</button>
           </div> : 
          <button onClick={nextQuestion} className="nextQuestionBtn">Next Question</button>}
